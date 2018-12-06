f1509800ec45040381baaa3dfde87ae2b732b002
rajendiran1997 <37899373+rajendiran1997@users.noreply.github.com>
2018-12-06 12:32:37 +0530

    Update ComputeJ.java

--- ComputeJ.java
+++ ComputeJ.java
@@ -4,7 +4,8 @@ import static java.lang.System.out;
 
 public class ComputeJ
 {
-
+	static double[] mean;
+	static double[] std; 
 	public double[][] transpose(double x[][])
 	{
 		double data[][] = new double[x[0].length][x.length];
@@ -64,6 +65,85 @@ public class ComputeJ
 			j += jval[i][0];
 		}
 		return j;
+	}
+
+	public double[] mean(double x[][])
+	{
+		double means[] = new double[x[0].length];
+		double data = 0;
+		for(int j = 1; j < x[0].length; j++)
+		{
+			for(int i = 0; i < x.length; i++)
+			{
+				data += x[i][j];
+			}
+			means[j-1] = (data/x.length);
+		}
+
+		return means;
+	}
+
+	public static double meanSquare(double data1, double data2)
+	{
+		double data = data1 - data2;
+		double square = data * data;
+		return square;
+	}
+
+	public double[] std(double x[][], double mean[])
+	{
+		double stde[] = new double[x[0].length];
+		double dx[][] = new double[x.length][(x[0].length-1)];
+		double sum = 0;
+		int row = x.length, col = x[0].length;
+		for(int j = 1; j < col; j++)
+		{
+			for(int i = 0; i < row; i++)
+			{
+				dx[i][(j-1)] = Math.pow((x[i][j] - mean[(j-1)]),2);
+			}
+
+			for(int k = 0; k < x.length; k++)
+			{
+				sum += dx[k][(j-1)];
+			}
+			// sum = (sum/x.length);
+			stde[(j-1)] = Math.sqrt((sum/(row-1)));
+		}
+		return stde;
+	}
+
+	public double[][] X_norm(double[][] x, double[] mean, double[] std)
+	{
+		for(int j = 1; j < x[0].length; j++)
+		{
+			for(int i = 0; i < x.length; i++)
+			{
+				x[i][j] = ((x[i][j] - mean[j-1])/std[j-1]);
+			}
+		}
+		return x;
+	}
+
+	public double[][] output(double[][] theta, double input[][])
+	{
+		ComputeJ ob = new ComputeJ();
+		double done[][] = new double[1][theta.length];
+		done[0][0] = 1.0;
+		for(int i = 0; i < input[0].length; i++)
+		{
+			done[0][(i+1)] = ((input[0][i] - mean[i])/std[i]);
+		}
+		double data[][] = ob.multiply(theta, done);
+		return data;
+	}
+
+	public double[][] x_norm(double x[][])
+	{
+		mean = mean(x);
+		std = std(x, mean);
+		x = X_norm(x, mean, std);
+		return x;
 	}
 
 	public double computeJ(double theta[][], double x[][], double y[][])
